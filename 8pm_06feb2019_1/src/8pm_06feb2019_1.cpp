b2cfe02e5f14a15938015cc4b328e9d293ef90ba
ONivia <ONivia@ONIVIA2019.grasco.com>
2019-02-06 20:31:37 -0500

    mi primer proyecto en Git.

---
+++ 8pm_06feb2019_1/src/8pm_06feb2019_1.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : 8pm_06feb2019_1.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "hola companeros 8pm!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
