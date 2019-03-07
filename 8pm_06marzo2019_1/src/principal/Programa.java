067783165d50d226c1975f983d7a9fc95842e7eb
ONivia <ONivia@ONIVIA2019.grasco.com>
2019-03-06 20:41:29 -0500

    pre/post y pesendencia de operadores.

--- 8pm_06marzo2019_1/src/principal/Programa.java
+++ 8pm_06marzo2019_1/src/principal/Programa.java
@@ -1,16 +1,17 @@
 package principal;
-public class Programa {
+public class Programa {	
 	public static void main(String[] args) {
 		//presedencia de operadores
 		//1. () 2. ++,-- 3. */% 4. +-
-		int r = 100 + 50 / 2 * 3;
-		//100 + 25 * 3
-		//100 + 75
-		//175
-		//CASO MAL:
-		//150 / 2 * 3
-		//75 * 3
-		//225
-		System.out.println(r);		
+		int i = 100;
+		int j = 200;
+		int r = 100 + 500 / (i++) * 2 + (j++) + (++i);
+		//100 + 500 / 100 * 2 + 200 + 102  =>i=102, j=201
+		//100 + 5 * 2 + 200 + 102
+		//100 + 10 + 200 + 102
+		//412
+		System.out.println(r);
+		System.out.println(i);
+		System.out.println(j);
 	}
 }
