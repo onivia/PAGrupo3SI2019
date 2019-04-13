a74ff889d0cc8343b334828833ad03aa95a6b7ce
ONivia <ONivia@ONIVIA2019.grasco.com>
2019-04-12 21:11:50 -0500

    ejemplo comportamiento clases: Inmutable Mutable

---
+++ 8pm_12abril2019_1/src/principal/Estudiante.java
@@ -0,0 +1,52 @@
+package principal;
+
+public class Estudiante {
+	public int codigo;
+	public String nombre;
+	public String direccion;
+	public String telefono;
+	
+	public Estudiante(int codigo, String nombre, String direccion) {
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.direccion = direccion;
+	}
+	
+	public Estudiante(int codigo, String nombre, String direccion, String telefono) {
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.direccion = direccion;
+		this.telefono = telefono;
+	}
+	
+	
+
+	@Override
+	public int hashCode() {
+		int resultado = 0;
+		int primo = 31;
+		
+		resultado = primo + String.valueOf(this.codigo).hashCode();
+		resultado = (primo * resultado) + this.nombre.hashCode();
+		resultado = (primo * resultado) + this.direccion.hashCode();
+		
+		return Math.abs(resultado);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		boolean sonIguales = false;
+		
+		if(obj!=null && obj.hashCode()==this.hashCode())
+			sonIguales = true;
+		
+		return sonIguales;
+	}
+	@Override
+	public String toString() {
+		String resultado = "";
+		resultado = "codigo: " + this.codigo + " nombre: " + this.nombre + " direccion: " + this.direccion + " telefono: " + this.telefono;
+		
+		return resultado;
+	}
+}
--- 8pm_12abril2019_1/src/principal/Programa.java
+++ 8pm_12abril2019_1/src/principal/Programa.java
@@ -2,15 +2,13 @@ package principal;
 
 public class Programa {
 	public static void main(String[] args) {
-		String s1 = "100";
-		String s2 = new String("100");
-		String s3 = String.valueOf(100);
-		String s4 = "100";
-		
-		if(s4.equals(s2)) {
-			System.out.println("son iguales");
-		} else {
-			System.out.println("NO son iguales");
-		}
+		Estudiante e1 = new Estudiante(100, "yo100", "A");
+		Estudiante e2 = e1;
+		e1.codigo = 200;
+		String s1 = new String("100");
+		String s2 = s1;
+		s1 = String.valueOf(500);
+		System.out.println(s1); //luisa-"500"; gilbert-"500"
+		System.out.println(s2); //luisa-"500"; g-"500"
 	}
 }
